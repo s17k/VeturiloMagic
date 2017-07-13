4d135e9ee98d8c7bfc1e0d02f8d67f3ab15d9d3c
s17k <stas.szczesniak@gmail.com>
2017-07-14 00:46:08 +0200

    Written adding data to the database and refractored the code

--- veturilo_parser.py
+++ veturilo_parser.py
@@ -1,6 +1,14 @@
 import requests
 import dataset
+import datetime
+import sqlalchemy
+import time
 from xml.dom import minidom
+
+
+DB_URL = 'sqlite:///veturilo.db'
+TABLE_NAME = 'bike_stops'
+
 
 def download_data():
     r = requests.get('https://nextbike.net/maps/nextbike-official.xml?city=210')
@@ -10,25 +18,86 @@ def download_data():
     veturilo_data.close()
 
 
-download_data()
+class BikeSeen:
+    def __init__(self, bike_id=None, station_id=None):
+        self.bike_id = bike_id
+        self.station_id = station_id
+        self.date = datetime.datetime.now()
 
-xmldoc = minidom.parse('veturilo.xml')
-itemlist = xmldoc.getElementsByTagName('place')
+    def add_bike_to_data_set(self, the_db, the_table):
+        sql_string = 'SELECT * FROM {} WHERE bike_id={} ORDER BY first_seen DESC LIMIT 1'\
+            .format(TABLE_NAME, self.bike_id)
 
-class Bike:
-    id=0
-    station_id=0
-    
+        results = [x for x in the_db.query(sql_string)]
+        result = None
+        if len(results) > 0:
+            result = results[0]
 
-bikes = []
+        data = dict()
+        data['bike_id']= self.bike_id
+        data['station_id']= self.station_id
+        data['last_seen']= self.date
+        data['times_seen'] = 1
 
-for s in itemlist:
-    if int(s.attributes['bikes'].value) == 0:
-        continue
-    print(s.attributes['name'].value, s.attributes['number'].value, s.attributes['bike_numbers'].value)
-    bike_numbers = s.attributes['bike_numbers'].value.split(',')
-    for num in bike_numbers:
-        print('Adding bike bike_id: {} station_id {}'.format(num, s.attributes['number'].value))
-        
+        if len(results)>0 and result['station_id'] == self.station_id:
+            data['first_seen']= result['first_seen']
+            data['last_seen']= self.date
+            data['times_seen']= result['times_seen'] + 1
+            the_table.update(data, ['bike_id', 'station_id', 'first_seen'])
+        else:
+            data['first_seen']= self.date
+            the_table.insert(data)
+
+        #print('Added seen bike bike_id:{} station_id:{} date:{}'
+        #      .format(self.bike_id, self.station_id, str(self.date)))
+
+
+def fetch_data_to_db():
+    download_data()
+    parsed_xml = minidom.parse('veturilo.dat')
+    stations = parsed_xml.getElementsByTagName('place')
+
+    bikes_now = []
+
+    for s in stations:
+        if int(s.attributes['bikes'].value) == 0:
+            continue
+
+        #print(s.attributes['name'].value, s.attributes['number'].value, s.attributes['bike_numbers'].value)
+
+        bike_numbers = s.attributes['bike_numbers'].value.split(',')
+
+        for num in bike_numbers:
+            bikes_now.append(BikeSeen(int(num), int(s.attributes['number'].value)))
+
+    with dataset.connect(DB_URL) as tx:
+        table = tx[TABLE_NAME]
+
+        if 'bike_id' not in table.columns:
+            table.create_column('bike_id', sqlalchemy.INT)
+
+        if 'first_seen' not in table.columns:
+            table.create_column('first_seen', sqlalchemy.DATETIME)
+
+        for bike_seen in bikes_now:
+            bike_seen.add_bike_to_data_set(tx, table)
+        try:
+            tx.commit()
+            print 'Successfully added/updated {} entries'.format(len(bikes_now))
+        except:
+            print 'Error Occurred'
+            tx.rollback()
+
+while True:
+    print('Fetching data from Veturilo API to db')
+    fetch_data_to_db()
+    print('Waiting 5 Seconds')
+    time.sleep(5)
+
+
+
+
+
+
     
 
