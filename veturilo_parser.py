5d691499f07ad3eca7d5ac011e1f4108e15e1c7a
s17k <stas.szczesniak@gmail.com>
2017-07-16 14:43:16 +0200

    Refactored the code and added observing the rented bikes

--- veturilo_parser.py
+++ veturilo_parser.py
@@ -7,7 +7,43 @@ from xml.dom import minidom
 
 
 DB_URL = 'sqlite:///veturilo.db'
-TABLE_NAME = 'bike_stops'
+BIKE_STOPS_TABLE_NAME = 'bike_stops'
+BIKE_IDS_TABLE_NAME = 'bike_ids'
+STATION420_ID = 420420
+
+
+def db_clear():
+    with dataset.connect(DB_URL) as tx:
+        bike_stops_table = tx[BIKE_STOPS_TABLE_NAME]
+        bike_ids_table = tx[BIKE_IDS_TABLE_NAME]
+
+        bike_stops_table.drop()
+        bike_ids_table.drop()
+
+        try:
+            tx.commit()
+        except:
+            tx.rollback()
+
+
+def db_init():
+    with dataset.connect(DB_URL) as tx:
+        bike_stops_table = tx[BIKE_STOPS_TABLE_NAME]
+        bike_ids_table = tx[BIKE_IDS_TABLE_NAME]
+
+        if 'bike_id' not in bike_stops_table.columns:
+            bike_stops_table.create_column('bike_id', sqlalchemy.INT)
+
+        if 'bike_id' not in bike_ids_table.columns:
+            bike_ids_table.create_column('bike_id', sqlalchemy.INT)
+
+        if 'first_seen' not in bike_stops_table.columns:
+            bike_stops_table.create_column('first_seen', sqlalchemy.DATETIME)
+
+        try:
+            tx.commit()
+        except:
+            tx.rollback()
 
 
 def download_data():
@@ -24,11 +60,16 @@ class BikeSeen:
         self.station_id = station_id
         self.date = datetime.datetime.now()
 
-    def add_bike_to_data_set(self, the_db, the_table):
-        sql_string = 'SELECT * FROM {} WHERE bike_id={} ORDER BY first_seen DESC LIMIT 1'\
-            .format(TABLE_NAME, self.bike_id)
+    def add_bike_to_data_set(self, transaction):
+        station_changes = 0
 
-        results = [x for x in the_db.query(sql_string)]
+        bike_stops_table = transaction[BIKE_STOPS_TABLE_NAME]
+        bike_ids_table = transaction[BIKE_IDS_TABLE_NAME]
+
+        sql_string = 'SELECT * FROM {} WHERE bike_id={} ORDER BY first_seen DESC LIMIT 1'\
+            .format(BIKE_STOPS_TABLE_NAME, self.bike_id)
+
+        results = [x for x in transaction.query(sql_string)]
         result = None
         if len(results) > 0:
             result = results[0]
@@ -43,10 +84,17 @@ class BikeSeen:
             data['first_seen']= result['first_seen']
             data['last_seen']= self.date
             data['times_seen']= result['times_seen'] + 1
-            the_table.update(data, ['bike_id', 'station_id', 'first_seen'])
+            bike_stops_table.update(data, ['bike_id', 'station_id', 'first_seen'])
         else:
+            station_changes += 1
             data['first_seen']= self.date
-            the_table.insert(data)
+            bike_stops_table.insert(data)
+
+        if bike_ids_table.find_one(bike_id=self.bike_id) is None:
+            bike_ids_table.insert({'bike_id': self.bike_id})
+            print 'Found a new bike with bike_id = {}'.format(self.bike_id)
+
+        return station_changes
 
 
 def fetch_data_to_db():
@@ -66,28 +114,47 @@ def fetch_data_to_db():
             bikes_now.append(BikeSeen(int(num), int(s.attributes['number'].value)))
 
     with dataset.connect(DB_URL) as tx:
-        table = tx[TABLE_NAME]
+        seen_ids = set([b.bike_id for b in bikes_now])
 
-        if 'bike_id' not in table.columns:
-            table.create_column('bike_id', sqlalchemy.INT)
+        all_bike_ids = [x['bike_id'] for x in tx[BIKE_IDS_TABLE_NAME].find()]
+        not_seen_bikes_ids = [x for x in all_bike_ids if x not in seen_ids]
+        station_changes = 0
 
-        if 'first_seen' not in table.columns:
-            table.create_column('first_seen', sqlalchemy.DATETIME)
+        for bike_id in not_seen_bikes_ids:
+            bikes_now.append(BikeSeen(bike_id, STATION420_ID))
 
         for bike_seen in bikes_now:
-            bike_seen.add_bike_to_data_set(tx, table)
+            station_changes += bike_seen.add_bike_to_data_set(tx)
+
         try:
             tx.commit()
-            print 'Successfully added/updated {} entries'.format(len(bikes_now))
+            print 'Successfully updated {} entries'.format(station_changes)
         except:
             print 'Error Occurred'
             tx.rollback()
 
+print 'Clearing Data from db'
+db_clear()
+
+print 'Initializing the db'
+db_init()
+
 while True:
-    print('Fetching data from Veturilo API to db')
+    time_start = time.time()
+
+    print 'Fetching data from Veturilo API to db'
     fetch_data_to_db()
-    print('Waiting 5 Seconds')
-    time.sleep(5)
+
+    time_finish = time.time()
+    time_elapsed = time_finish-time_start
+
+    print 'Done in {} seconds'.format(time_elapsed)
+
+    time_to_wait = max(0.0, 45.0-time_elapsed)
+
+    print 'Waiting {} Seconds'.format(time_to_wait)
+
+    time.sleep(time_to_wait)
 
 
 
