e349b43b927203e4b0ee8656efa8c934603f4ef8
s17k <stas.szczesniak@gmail.com>
2017-07-31 23:50:29 +0200

    Added priors and better logs

--- veturilo_parser.py
+++ veturilo_parser.py
@@ -5,17 +5,18 @@ from xml.dom import minidom
 import dataset
 import requests
 import sqlalchemy
+import math
 
 DB_URL = 'sqlite:///veturilo.db'
 BIKE_STOPS_TABLE_NAME = 'bike_stops'
-BIKE_IDS_TABLE_NAME = 'bike_ids'
+BIKES_TABLE_NAME = 'bikes'
 STATION420_ID = 420420
-
+PRIORS = 1.0/99.0
 
 def db_clear():
     with dataset.connect(DB_URL) as tx:
         bike_stops_table = tx[BIKE_STOPS_TABLE_NAME]
-        bike_ids_table = tx[BIKE_IDS_TABLE_NAME]
+        bike_ids_table = tx[BIKES_TABLE_NAME]
 
         bike_stops_table.drop()
         bike_ids_table.drop()
@@ -29,19 +30,19 @@ def db_clear():
 def db_init():
     with dataset.connect(DB_URL) as tx:
         bike_stops_table = tx[BIKE_STOPS_TABLE_NAME]
-        bike_ids_table = tx[BIKE_IDS_TABLE_NAME]
+        bike_ids_table = tx[BIKES_TABLE_NAME]
 
-        if 'bike_id' not in bike_stops_table.columns:
-            bike_stops_table.create_column('bike_id', sqlalchemy.INT)
+        def create_column(column_name, column_type, table):
+            if column_name not in table.columns:
+                table.create_column(column_name, column_type)
 
-        if 'bike_id' not in bike_ids_table.columns:
-            bike_ids_table.create_column('bike_id', sqlalchemy.INT)
-
-        if 'first_seen' not in bike_stops_table.columns:
-            bike_stops_table.create_column('first_seen', sqlalchemy.DATETIME)
+        create_column('bike_id', sqlalchemy.INT, bike_stops_table)
+        create_column('bike_id', sqlalchemy.INT, bike_ids_table)
+        create_column('first_seen', sqlalchemy.DATETIME, bike_stops_table)
 
         bike_ids_table.create_index(['bike_id'])
         bike_stops_table.create_index(['bike_id', 'station_id', 'first_seen'])
+        bike_stops_table.create_index(['bike_id'])
 
         try:
             tx.commit()
@@ -53,7 +54,7 @@ def download_data():
     r = requests.get('https://nextbike.net/maps/nextbike-official.xml?city=210')
     assert r.status_code == 200
     veturilo_data = open('veturilo.dat', "w")
-    veturilo_data.write(r.content)
+    veturilo_data.write(r.content.decode("utf-8"))
     veturilo_data.close()
 
 
@@ -63,8 +64,9 @@ class BikeSeen:
         self.station_id = station_id
         self.date = datetime.datetime.now()
 
-    def find_last_occurrence_in_db(transaction):
-        sql_string = 'SELECT * FROM {} WHERE bike_id={} ORDER BY first_seen DESC LIMIT 1' \
+    def find_last_occurrence_in_db(self, transaction):
+        sql_string = 'SELECT * FROM {} WHERE bike_id={}'\
+        ' ORDER BY first_seen DESC LIMIT 1' \
             .format(BIKE_STOPS_TABLE_NAME, self.bike_id)
 
         results = [x for x in transaction.query(sql_string)]
@@ -77,12 +79,11 @@ class BikeSeen:
         return result
 
     def add_bike_to_data_set(self, transaction):
-        station_changes = 0
 
         bike_stops_table = transaction[BIKE_STOPS_TABLE_NAME]
-        bike_ids_table = transaction[BIKE_IDS_TABLE_NAME]
+        bike_ids_table = transaction[BIKES_TABLE_NAME]
 
-        last_occurrence = BikeSeen.find_last_occurrence_in_db(transaction)
+        last_occurrence = self.find_last_occurrence_in_db(transaction)
 
         data = dict()
         data['bike_id'] = self.bike_id
@@ -94,17 +95,22 @@ class BikeSeen:
             data['first_seen'] = last_occurrence['first_seen']
             data['last_seen'] = self.date
             data['times_seen'] = last_occurrence['times_seen'] + 1
-            bike_stops_table.update(data, ['bike_id', 'station_id', 'first_seen'])
+
+            search_columns = ['bike_id', 'station_id', 'first_seen']
+            bike_stops_table.update(data, search_columns)
+            update_type = 0
         else:
-            station_changes += 1
             data['first_seen'] = self.date
             bike_stops_table.insert(data)
+            update_type = 1
 
-        if bike_ids_table.find_one(bike_id=self.bike_id) is None:
-            bike_ids_table.insert({'bike_id': self.bike_id})
-            print('Found a new bike with bike_id = {}'.format(self.bike_id))
+            if bike_ids_table.find_one(bike_id=self.bike_id) is None:
+                insert_data = {'bike_id': self.bike_id, 'broken_pbb': PRIORS}
+                bike_ids_table.insert(insert_data)
+                # print('Found a new bike with bike_id = {}'.format(self.bike_id))
+                update_type = 2
 
-        return station_changes
+        return update_type
 
 
 def fetch_data_to_db():
@@ -114,31 +120,49 @@ def fetch_data_to_db():
 
     bikes_now = []
 
-    for s in stations:
-        if int(s.attributes['bikes'].value) == 0:
+    for station in stations:
+        if int(station.attributes['bikes'].value) == 0:
             continue
 
-        bike_numbers = s.attributes['bike_numbers'].value.split(',')
+        bike_numbers = station.attributes['bike_numbers'].value.split(',')
+        station_number = int(station.attributes['number'].value)
 
-        for num in bike_numbers:
-            bikes_now.append(BikeSeen(int(num), int(s.attributes['number'].value)))
+        for bike_number in bike_numbers:
+            int_num = int(bike_number)
+            bikes_now.append(BikeSeen(int_num, station_number))
 
     with dataset.connect(DB_URL) as tx:
         seen_ids = set([b.bike_id for b in bikes_now])
 
-        all_bike_ids = [x['bike_id'] for x in tx[BIKE_IDS_TABLE_NAME].find()]
+        all_bike_ids = [x['bike_id'] for x in tx[BIKES_TABLE_NAME].find()]
         not_seen_bikes_ids = [x for x in all_bike_ids if x not in seen_ids]
-        station_changes = 0
 
         for bike_id in not_seen_bikes_ids:
             bikes_now.append(BikeSeen(bike_id, STATION420_ID))
 
-        for bike_seen in bikes_now:
-            station_changes += bike_seen.add_bike_to_data_set(tx)
+        update_type_counters = [0 for i in range(3)]
 
+        for bike_seen in bikes_now:
+            update_type = bike_seen.add_bike_to_data_set(tx)
+            update_type_counters[update_type] += 1
         try:
             tx.commit()
-            print('Successfully updated {} entries'.format(station_changes))
+            print('Data-fetch logs:')
+
+            station_updates = update_type_counters[1]
+            print('\tBikes that changed station: {}'.format(station_updates))
+
+            new_bikes = update_type_counters[2]
+            print('\tBikes seen for the first time: {}'.format(new_bikes))
+
+            same_station = update_type_counters[0]
+            print('\tstayed at the same station: {}'.format(same_station))
+
+            rented_bikes = tx.query('SELECT * FROM {} WHERE station_id={}'\
+                               .format(BIKE_STOPS_TABLE_NAME, STATION420_ID))
+
+            print('\tknown rented bikes: {}'\
+                  .format(len([x for x in rented_bikes])))
         except:
             print('Error Occurred')
             tx.rollback()
@@ -161,7 +185,7 @@ while True:
 
     print('Done in {} seconds'.format(time_elapsed))
 
-    time_to_wait = max(0.0, 45.0 - time_elapsed)
+    time_to_wait = max(0.0, 60.0 - time_elapsed)
 
     print('Time now {}, waiting {} Seconds'.format(datetime.datetime.now().time(), time_to_wait))
 
@@ -170,7 +194,7 @@ while True:
         while True:
             print('{} seconds left'.format(duration))
             time.sleep(min(10.0, duration))
-            duration -= 10.0
+            duration = 60.0 - time.time() + time_start
             if duration <= 0.0:
                 break
 
